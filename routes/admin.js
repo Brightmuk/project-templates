9458792e42f04904c35521025fa39bb2cd160a53
Brightmuk <be.rightmuk@gmail.com>
2022-11-21 16:15:53 +0300

    Remove unused files

--- routes/admin.js
+++ routes/admin.js
@@ -8,47 +8,25 @@ const adminControler = require('../controllers/admin');
 
 
 router.route('/')
-   .get(adminControler.getLogin) //get request
-   .post(adminControler.postLogin) // post request
+   .get(adminControler.getLogin) 
+   .post(adminControler.postLogin) 
 
 router.route('/cars') 
-      .get(adminControler.getCars)//get pending
+      .get(adminControler.getCars)
 
 
-router.get('/logout',adminControler.logout) //get request 
+router.get('/logout',adminControler.logout)  
 
 router.route('/addCar')
-      .get(adminControler.getAddCar) // get request for hotel add page
-      .post(adminControler.postAddCar) // post request for hotel add to db
+      .get(adminControler.getAddCar) 
+      .post(adminControler.postAddCar) 
  
 
-
-
-
-router.route('/pending')
-      .get(adminControler.getPending)//get pending
-
-router.route('/reservations')
-      .get(adminControler.getReservations)//get reservations
-
-router.post('/changeStatus',adminControler.postChangeStatus)// post change status
-
-
-
-router.route('/search')
-      .get(adminControler.getSearch)   // get request   
-      .post(adminControler.postSearch) // post request
-
 router.route('/viewRoom')
-      .post(adminControler.viewRoom) //view room page
-
-router.route('/viewReservation')
-      .post(adminControler.viewReservation) //view room page
-router.route('/checkout')
-      .post(adminControler.checkout) //view room page  
+      .post(adminControler.viewRoom)  
 
 router.route('/updateRoom')
-      .post(adminControler.updateRoom) // update prev data      
+      .post(adminControler.updateRoom)   
 
-
+ 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -13,35 +13,9 @@ router.route('/search')
        .post(userControler.postSearch)
 
 router.route('/compare')
-       .post(userControler.postCompare) 
-
- 
-
- 
-
-router.route('/login')
-       .get(userControler.getLogin) // get request for login
-       .post(userControler.postLogin)// post request for login
-
-router.route('/createaccount') 
-       .get(userControler.getCreateAccount)    //get request for create account   
-       .post(userControler.postCreateAccount); //post request for create account   
-
-
-        
-router.route('/boooking')
-       .post(userControler.postBooking) //post booking data    
-        
-router.route('/status') 
-       .post(userControler.postStatus); 
-
-router.route('/showStatus')
-       .get(userControler.authentication,userControler.getShowStatus);// get show status
-
-router.post('/deletereq',userControler.deleteBooking,userControler.getShowStatus);       
+       .post(userControler.postCompare)      
        
 router.get('/contact',userControler.getContact);       
-
-router.get('/logout',userControler.logout); //logout       
+     
 
 module.exports = router;
